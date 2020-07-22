d7f5144766ee97bbca7d99153bc98c28db41f509
alexey-troyan <prime891891@gmail.com>
2020-07-22 16:16:26 +0300

    add Parallax to newsletter section

--- js/main.js
+++ js/main.js
@@ -1,16 +1,15 @@
+$('.newsletter').parallax({ imageSrc: '../img/newsletter-bg.jpg' });
 var mySwiper = new Swiper('.swiper-container', {
   // Optional parameters
-  
   loop: true,
-
   // Navigation arrows
   navigation: {
     nextEl: '.slider-button--next',
     prevEl: '.slider-button--prev',
   },
-
   keyboard: {
     enabled: true,
     onlyInViewport: false,
   },
-})
+});
+
